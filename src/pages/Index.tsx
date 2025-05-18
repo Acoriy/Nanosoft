1b33e630020399a0ca39253ec9f072807ff5ab3d
Acoriy <sofayne.acoriy@gmail.com>
2025-05-18 13:09:05 +0100

    Laste

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -126,7 +126,7 @@ const Index = () => {
         {/* <div className="absolute inset-0 bg-gradient-radial from-transparent to-white/70 dark:to-gray-900/30">
         
         </div> */}
-        <div className="container mx-auto px-4 z-10 py-12 md:py-20 w-auto h-auto">
+        <div className="container  px-12 md:px-20 z-10 py-12 md:py-20 w-auto h-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12 items-center">
             {/* Text Content */}
             <motion.div
